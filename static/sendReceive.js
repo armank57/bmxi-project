b413e8d864c45e4c2de3a97bc78bfefd951e0b25
David <cox407@purdue.edu>
2024-01-20 16:20:32 -0500

    updating connection and sendreceive

--- static/sendReceive.js
+++ static/sendReceive.js
@@ -11,26 +11,20 @@ function receiveData(callback) {
         callback(text);
     });
 }
-
+/*
 $(document).ready(function () {
     // Button was clicked
     document.getElementById("send_button").onclick = function () {
         // Get the text value
-        var text = document.getElementById("textfield_input").value;
-
-        // Update the chat window
-        document.getElementById("chat").innerHTML += "You: " + text + "\n\n";
+        var text;
 
         // Emit a message to the 'send_message' socket
         sendData(text)
-
-        // Set the textfield input to empty
-        document.getElementById("textfield_input").value = "";
-    }
 
     // Message received from server
     receiveData(function (receivedText) {
     // Now you can use the received text here
     console.log("Received Text: " + receivedText);
     });
+    */
 });
