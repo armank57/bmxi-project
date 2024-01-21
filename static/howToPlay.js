1b675f17b981b6f4e4115a92893ef7e921dde79b
armank57 <112572260+armank57@users.noreply.github.com>
2024-01-21 07:52:38 -0500

    put in rules

--- static/howToPlay.js
+++ static/howToPlay.js
@@ -1,19 +1,11 @@
+function preload() {
+    rules = loadImage("static/instructions.png");
+}
+
 function setup() {
     createCanvas(400, 400);
 }
 
 function draw() {
-    background(220);
-    
-    // Set text properties
-    textFont('Helvetica');
-    textSize(20);
-    textAlign(CENTER);
-    fill(0);
-    
-    // Display the rules
-    text("Game Rules:", width / 2, height / 2 - 40);
-    text("- Rule 1", width / 2, height / 2);
-    text("- Rule 2", width / 2, height / 2 + 40);
-    text("- Rule 3", width / 2, height / 2 + 80);
+    image(rules, w/2 - 200, h/2 - 200, 400, 400);
 }
