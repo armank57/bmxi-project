465ff56bf1db78580dd13f2dd6cf2cb75c19e2c4
StevenSchonlau <sschonla@purdue.edu>
2024-01-21 06:06:43 -0500

    FIxed unlimited food bug

--- static/HostJS/drawMap.js
+++ static/HostJS/drawMap.js
@@ -86,6 +86,9 @@ function draw_map(w, h) {
                 } else if (textures[row][col] == 2 && ownership[row][col] == 3) {
                     players[3].stone = players[3].stone + 2;
                 }
+                if (col == mapSize-1 && row == mapSize -1) {
+                    flag = true;
+                }
 
                 
             } else if (currentTime % 5 != 0) {
