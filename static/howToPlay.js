fe046edb7edd3edaf9f5dd09f6f3f3944572d07e
armank57 <112572260+armank57@users.noreply.github.com>
2024-01-21 07:56:41 -0500

    fixed h2p

--- static/howToPlay.js
+++ static/howToPlay.js
@@ -1,9 +1,23 @@
+function updateContainer() {
+    container = select('#sketchContainer');
+    w = parseFloat(getComputedStyle(container.elt).getPropertyValue('width'));
+    h = parseFloat(getComputedStyle(container.elt).getPropertyValue('height'));
+  }
+
+  function windowResized() {
+    updateContainer();
+    resizeCanvas(w, h);
+    drawing = true;
+  }
+  
 function preload() {
     rules = loadImage("static/instructions.png");
 }
 
 function setup() {
-    createCanvas(400, 400);
+    updateContainer();
+    canvas = createCanvas(w, h);
+    canvas.parent("#sketchContainer");
 }
 
 function draw() {
